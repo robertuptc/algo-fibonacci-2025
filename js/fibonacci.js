9205593c199d664025277fc649e579078e56c48b
robertuptc <robertuptc@gmail.com>
2025-03-03 11:51:55 -0500

    js finished

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = (num) => {
-    
+    let fibonacciSequence = [0, 1];
+
+    if (num === 0 || num === 1) {
+        return fibonacciSequence[num]
+    } else {
+        let prev = fibonacciSequence[0];
+        let current = 0;
+
+        for (let i = 1; i <= fibonacciSequence.length; i++) {
+            if (fibonacciSequence.length != num + 1) {
+                current = prev + fibonacciSequence[i];
+                prev = fibonacciSequence[i]
+                fibonacciSequence.push(current);
+            }    
+        }
+        return fibonacciSequence.pop()
+    }
 }
 
 module.exports = {fibonacci}
